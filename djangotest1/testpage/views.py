122dda8ebde155b9e44ea32ab10600a80032b4ac
James Kelly <james@Jamess-MacBook-Pro.local>
2024-02-23 22:41:26 +0000

    add modals

--- djangotest1/testpage/views.py
+++ djangotest1/testpage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def home(request):
-    return render(request,'testpage/index.html')
+    items = Item.objects.all()
+    return render(request,'testpage/index.html',context={
+        'items':items
+    })
     
